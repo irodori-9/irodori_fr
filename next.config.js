c0889e3b428075204b91757aefbaffdd2c0598dd
MitsuruMe <mitsurunano@gmail.com>
2025-08-26 21:30:16 +0900

    はなすぼたんをおしたときたなぶたちゃんが表示されない問題修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone', // ★ 追加（AppServiceでは非常に重要）
+  
+  images: {
+    unoptimized: true, // Azure App Service standalone モードでの画像表示対応
+  },
 
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
